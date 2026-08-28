16b5073caba58be6e17642d9eac522153a4e7247
Sai Sri N <saisrinirmal@gmail.com>
2026-08-28 14:54:05 +0530

    summary

--- frontend/js/summary.js
+++ frontend/js/summary.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
-
+const API_BASE_URL = "https://intellibusiness-db.onrender.com";
 document.addEventListener('DOMContentLoaded', () => {
     loadSummary();
     bindActions();
