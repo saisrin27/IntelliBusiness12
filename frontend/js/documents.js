a4b895569926d926722d2e9f229d99de4828e551
Sai Sri N <saisrinirmal@gmail.com>
2026-08-28 14:45:43 +0530

    doc fix

--- frontend/js/documents.js
+++ frontend/js/documents.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadDocuments();
 });
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = "https://intellibusiness-db.onrender.com";
 const state = {
     selectedFiles: [],
     documents: [],
